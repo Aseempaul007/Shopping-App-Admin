31ac49b11663ac088e93fec0a16797cc302025fd
aseempal007 <aseempal007@gmail.com>
2023-10-18 13:26:58 +0530

    Project created!

---
+++ app/src/main/java/com/example/shoppingappadmin/data/remote/model/ProductModel.kt
@@ -0,0 +1,9 @@
+package com.example.shoppingappadmin.data.remote.model
+
+//model class for product
+data class ProductModel(
+    var productImage: String,
+    var productPrice: Int,
+    var productName: String,
+    var productDetails: String
+)
